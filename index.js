edfa06ff220f7a78af71bdebc4878195027c417d
Scary-Terrance <bnv12310@ccv.vsc.edu>
2018-11-29 23:59:27 -0500

    Added basic DOM parsing with request-promise and cheerio.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-node_modules/
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+var url = 'https://anrweb.vt.gov/DEC/WWInventory/SewageOverflows.aspx';
+
+rp(url)
+    .then(function (html) {
+        // Proccess the HTML
+        const $ = cheerio.load(html);
+        // Load the headers reviewed by the DEC Wastewater Staff
+        var reviewed_headers = [];
+        $("#body_GridViewSewageOverflowsAuthorized .dataList_header th a").each(function(i, elem) {
+            headers[i] = $(this).text();
+            console.log($(this).text());
+        });
+    })
+    .catch(function (err) {
+        // Crawling failed...
+        console.log("Error Accessing webpage: " + err);
+    });
